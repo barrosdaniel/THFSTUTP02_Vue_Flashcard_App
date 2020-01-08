7fa2fbd62268be097bd87ed17f7cf4c1553d9ec0
barros.daniel@outlook.com <barros.daniel@outlook.com>
2020-01-08 22:41:16 +0800

    feature: Add new card with information from the the form input fields

--- app.js
+++ app.js
@@ -25,18 +25,32 @@ new Vue({
   el: '#flashcard-app',
   data: {
     cards: cards,
+    newFront: '',
+    newBack: ''
   },
   methods: {
     flipCard: function (card) {
       card.flipped = !card.flipped;
+    },
+    addCard: function () {
+      const newCard = {
+        front: this.newFront,
+        back: this.newBack,
+        flipped: false
+      };
+      this.cards.push(newCard);
     }
   }
 });
 
-// 1 - Display card data
-// 2 - Flip cards on click
-// 3 - Get info for new cards from the user
+// DONE 1 - Display card data
+// DONE 2 - Flip cards on click
+// DONE 3 - Get info for new cards from the user
+// DONE 3.1 - Create properties for the front and back of the cards
+// DONE 3.2 - Bind the new properties to the form inputs using v-model
 // 4 - Add new card when the user hits enter or press the button
+// 4.1 - Create new card object with new card information (from form)
+// 4.2 - Add new card to cards array
 // 5 - Delete cards
 // 6 - Animate card flip
 // 7 - Display error message if form fields are left blank
