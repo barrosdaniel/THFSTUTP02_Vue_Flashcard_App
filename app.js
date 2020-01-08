886766f03e2de47eb279b0645dced3f2c42db85b
barros.daniel@outlook.com <barros.daniel@outlook.com>
2020-01-08 21:55:13 +0800

    feature: Add data to card component

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const cards = [
-    {
-      front: 'The "First Computer Programmer"',
-      back: 'Ada Lovelace',
-      flipped: false,
-    },
-    {
-      front: 'Invented the "Clarke Calculator"',
-      back: 'Edith Clarke',
-      flipped: false,
-  
-    },
-    {
-      front: 'Famous World War II Enigma code breaker',
-      back: 'Alan Turing',
-      flipped: false,
-    },
-    {
-      front: 'Created satellite orbit analyzation software for NASA',
-      back: 'Dr. Evelyn Boyd Granville',
-      flipped: false,
-    },
-  ]; 
+const cards = [{
+    front: 'The "First Computer Programmer"',
+    back: 'Ada Lovelace',
+    flipped: false,
+  },
+  {
+    front: 'Invented the "Clarke Calculator"',
+    back: 'Edith Clarke',
+    flipped: false,
 
-  new Vue({
-    el: '#flashcard-app',
-    data: {
-      cards: cards,
-    },
-    methods: {
+  },
+  {
+    front: 'Famous World War II Enigma code breaker',
+    back: 'Alan Turing',
+    flipped: false,
+  },
+  {
+    front: 'Created satellite orbit analyzation software for NASA',
+    back: 'Dr. Evelyn Boyd Granville',
+    flipped: false,
+  },
+];
 
-    }
-  });
+new Vue({
+  el: '#flashcard-app',
+  data: {
+    cards: cards,
+  },
+  methods: {
+
+  }
+});
+
+// 1 - Display card data
+// 2 - Flip cards on click
+// 3 - Get info for new cards from the user
+// 4 - Add new card when the user hits enter or press the button
+// 5 - Delete cards
+// 6 - Animate card flip
+// 7 - Display error message if form fields are left blank
